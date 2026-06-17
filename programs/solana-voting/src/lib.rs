0bf36525f259663d88e6576b70fdfcae58222969
0xSardius <0xSardius@gmail.com>
2026-06-17 17:16:00 -0400

    complete init_poll fn

--- programs/solana-voting/src/lib.rs
+++ programs/solana-voting/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use std::str::pattern::StrSearcher;
 
 declare_id!("BTzpSv33D4FcRcGT8urd1MJF9SkN3sneRD21e7hB8v86");
 
@@ -6,15 +7,32 @@ declare_id!("BTzpSv33D4FcRcGT8urd1MJF9SkN3sneRD21e7hB8v86");
 pub mod solana_voting {
     use super::*;
 
-    pub fn init_poll(ctx: Context<InitPoll>) -> Result<()> {
-        init_poll::handler(ctx);
+    pub fn init_poll(ctx: Context<InitPoll>, poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
+        let mut poll = ctx.accounts.poll_account;
+        poll.poll_description = description;
+        poll.poll_voting_start = start;
+        poll.poll_voting_end = end;
+        poll.poll_name = name;
+        poll.poll_option_index = 0;
+        Ok(())
     }
 }
 
 #[derive(Accounts)]
+#[instruction(poll_id: u64)]
 pub struct InitPoll {
     #[account(mut)]
-    pub signer: Signer<'info>
+    pub signer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + PollAccount::INIT_SPACE,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()], bump,
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[account]
