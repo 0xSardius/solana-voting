2889f6fdd4a921bb04e7a57e80d8c8daeecd0a2a
0xSardius <0xSardius@gmail.com>
2026-06-16 20:49:25 -0400

    PollAccount, Candidate Poll, and InitPoll (parital) created

--- programs/solana-voting/src/lib.rs
+++ programs/solana-voting/src/lib.rs
@@ -6,11 +6,33 @@ declare_id!("BTzpSv33D4FcRcGT8urd1MJF9SkN3sneRD21e7hB8v86");
 pub mod solana_voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn init_poll(ctx: Context<InitPoll>) -> Result<()> {
+        init_poll::handler(ctx);
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitPoll {
+    #[account(mut)]
+    pub signer: Signer<'info>
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct PollAccount(
+    #[max_len(32)]
+    pub poll_name: String,
+    #[max_len(280)]
+    pub poll_description: String,
+    pub poll_voting_start: u64,
+    pub poll_voting_end: u64,
+    pub poll_option_index: u64,
+)
+
+#[account]
+#[derive(InitSpace)]
+pub struct CandidateAccount {
+    #[max_len(32)]
+    pub candidate_name: String,
+    pub candidate_votes: u64,
+}
