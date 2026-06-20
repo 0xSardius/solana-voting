9c04811cf866097836986aa6b1c4e0d4c73032b0
0xSardius <0xSardius@gmail.com>
2026-06-20 13:19:53 -0400

    init candidate instreuctions and Vote struct

--- programs/solana-voting/src/lib.rs
+++ programs/solana-voting/src/lib.rs
@@ -7,14 +7,26 @@ declare_id!("BTzpSv33D4FcRcGT8urd1MJF9SkN3sneRD21e7hB8v86");
 pub mod solana_voting {
     use super::*;
 
-    pub fn init_poll(ctx: Context<InitPoll>, poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
-        let mut poll = ctx.accounts.poll_account;
+    pub fn init_poll(ctx: Context<InitPoll>, _poll_id: u64, start: u64, end: u64, name: String, description: String) -> Result<()> {
+        let poll = &mut Account<' _, PollAccount> = & mut ctx.accounts.poll_account;
         poll.poll_description = description;
         poll.poll_voting_start = start;
         poll.poll_voting_end = end;
         poll.poll_name = name;
         poll.poll_option_index = 0;
         Ok(())
+    }
+
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>, 
+    _poll_id: u64,
+    candidate: String) -> Result<()> {
+        ctx.accounts.candidate_account.candidate_name = candidate;
+        ctx.accounts.poll_account.poll_option_index += 1;
+        Ok(())
+    }
+
+    pub fn vote(ctx: Context<Vote>) -> Result<()> {
+        
     }
 }
 
@@ -56,6 +68,25 @@ pub struct InitializeCandidate {
     pub candidate_account: Account<'info, CandidateAccount>,
 
     pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(poll_id: u64, candidate: String)]
+pub struct Vote {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"poll".as_ref(), poll_id.to_le_bytes().as_ref()], bump,
+    )]
+    pub poll_account: Account<'info, PollAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"candidate".as_ref(), poll_id.to_le_bytes().as_ref(), candidate.as_ref()], bump,
+    )]
+    pub candidate_account: Account<'info, CandidateAccount>,
 }
 
 #[account]
